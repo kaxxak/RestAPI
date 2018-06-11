bd98c3ac47d376184805ce9de61e7676a3339206
sevoGrigoryan <40152114+sevoGrigoryan@users.noreply.github.com>
2018-06-11 05:45:33 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const zendesc = require('./zendesk/zendesk')
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const PORT = process.env.PORT || 5000;
 
-var app = express();
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,7 +39,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(process.env.PORT || 5000, function() {
-  console.log("Node server running on http://localhost:3000");
-});
+app.listenlisten(PORT, () => console.log(`Listening on ${ PORT }`));
 module.exports = app;
