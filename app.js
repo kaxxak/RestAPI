df2996572882dd782f460c70370b05380e6de2ac
Sevo Grigoryan <you@example.com>
2018-06-11 05:36:34 +0400

    First commit

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(3000, function() {
+app.listen(process.env.PORT || 5000, function() {
   console.log("Node server running on http://localhost:3000");
 });
 module.exports = app;
